d39e23f8af8c1bb75c4137eb0a16aefe050c556e
Connor Rooney <connorrooney.ap@gmail.com>
2019-03-26 10:21:43 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList.js';
+import TodoItems from './TodoItems.js';
 
 class App extends Component {
+  state = {
+    items: [],
+    currentItem: {text:'', key:''},
+  }
+
+  handleInput = e => {
+   const itemText = e.target.value
+   const currentItem = {text: itemText, key: Date.now()}
+   this.setState({currentItem})
+  }
+
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== "") {
+      console.log(newItem)
+      const items = [...this.state.items, newItem]
+      this.setState({items: items, currentItem: {text: '', key: ''}})
+    }
+  }
+
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => { 
+      return item.key !== key
+    })
+    this.setState({items: filteredItems})
+  }
+
+  inputElement = React.createRef()
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+            <h1>Todo List</h1>
+            <h3>Type and Click to add Tasks.</h3>
+            <h3>Click on a task to remove it</h3>
+          <TodoList addItem={this.addItem} inputElement={this.inputElement} handleInput={this.handleInput} currentItem={this.state.currentItem}/>
+          <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
+        </div>
       </div>
     );
   }
