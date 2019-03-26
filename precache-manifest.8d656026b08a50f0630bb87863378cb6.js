ed176946c47f59ef88d4c34e16ca276f0bcb3428
Connor Rooney <connorrooney.ap@gmail.com>
2019-03-26 10:30:46 +0000

    Updates

---
+++ precache-manifest.8d656026b08a50f0630bb87863378cb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe35fca0fea852899367",
+    "url": "/react_todo/static/js/runtime~main.db2a12cb.js"
+  },
+  {
+    "revision": "b4b856151b764e15776f",
+    "url": "/react_todo/static/js/main.9a8bdc33.chunk.js"
+  },
+  {
+    "revision": "5883fe98142232124cba",
+    "url": "/react_todo/static/js/2.97e2f7ef.chunk.js"
+  },
+  {
+    "revision": "b4b856151b764e15776f",
+    "url": "/react_todo/static/css/main.da147786.chunk.css"
+  },
+  {
+    "revision": "25d291e8b193a0f0363061d3ebc65921",
+    "url": "/react_todo/index.html"
+  }
+];
